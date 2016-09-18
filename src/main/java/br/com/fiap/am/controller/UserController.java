7b9164be0527a472fd473b4d622e4e6530bc5749
Marcelo Nidal <marcelonidal@gmail.com>
2016-09-18 19:47:19 -0300

    inicio do projeto, conexão com o tomcat

--- src/main/java/br/com/fiap/am/controller/UserController.java
+++ src/main/java/br/com/fiap/am/controller/UserController.java
@@ -2,6 +2,8 @@ package br.com.fiap.am.controller;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Post;
+import br.com.fiap.am.model.User;
 
 /**
  * Created by felipeweb on 12/09/16.
@@ -11,5 +13,17 @@ public class UserController {
     @Get("/user/form")
     public void form() {
 
+    }
+    
+    @Post("/user/save")
+    public void saveUser(User user){
+    	
+    }
+    
+    @Get("/user/edit/{id}")
+    public void edit(Long id){
+    	user  = findById(id)
+    	result.include("user", user)
+    	result.forward(this).form()
     }
 }
--- src/main/webapp/WEB-INF/jsp/user/form.jsp
+++ src/main/webapp/WEB-INF/jsp/user/form.jsp
@@ -9,6 +9,13 @@
     <div class="container-fluid">
         <h1 class="ls-title-intro ls-ico-user"><fmt:message key="new.user"/></h1>
         <tags:messages/>
+        <form method="post" action="/user/save" >
+        	<input type="hidden" name="user.id">
+        	<input type="text" name="user.name" value="${user.name}">
+        	<input type="text" name="user.login">
+        	<input type="text" name="user.password">
+        	<select name="user.accessLevel"></select>
+        </form>
     </div>
 </main>
 <tags:footer/>
